eed6a00bcb71668c768fc22c465df921b2456637
tekky02 <tekkykk02@gmail.com>
2025-06-25 21:22:28 +0800

    enable linger for vm users

--- hosts/vm/user.nix
+++ hosts/vm/user.nix
@@ -2,8 +2,11 @@
 {
   users.users.${username} = {
     isNormalUser = true;
+		# enable lingering for this user
+		# see https://www.freedesktop.org/software/systemd/man/latest/loginctl.html#:~:text=Enable%2Fdisable%20user%20lingering%20for,to%20run%20long%2Drunning%20services.
+		linger = true;
     extraGroups = [
-      "whell"
+      "wheel"
       "podman"
     ];
     shell = pkgs.fish;
